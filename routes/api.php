1f783bf843d4a87e79e1bb4d9d30cbe12c5df6aa
ongoni <ongoni.fg@gmail.com>
2022-04-02 20:45:09 +0400

    add routing

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('author', \App\Http\Controllers\AuthorController::class);
+
+Route::apiResource('book', \App\Http\Controllers\BookController::class);
