42cb8909c2daa1193059195ccedd0ad18d601bab
ongoni <ongoni.fg@gmail.com>
2022-04-03 21:04:21 +0400

    group api resource routes

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('author', \App\Http\Controllers\AuthorController::class);
-
-Route::apiResource('book', \App\Http\Controllers\BookController::class);
+Route::apiResources([
+    'author' => \App\Http\Controllers\AuthorController::class,
+    'book' => \App\Http\Controllers\BookController::class
+]);
