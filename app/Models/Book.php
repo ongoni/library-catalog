57c731a4c025da892e7f36d5ec5ec5cd703e0685
ongoni <ongoni.fg@gmail.com>
2022-04-03 19:11:08 +0400

    change publisher to edition

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,13 +2,39 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * Class Book
+ * @package App\Models
+ * @mixin Builder
+ */
 class Book extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
-    protected $fillable = ['title', 'publisher', 'publication_date'];
+    protected $fillable = ['title', 'edition'];
+
+//    protected $hidden = ['created_at', 'updated_at', 'deleted_at'];
+
+    /**
+     * Get related author
+     *
+     * @return BelongsTo
+     */
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(Author::class);
+    }
+
+    public function setAuthorAttribute()
+    {
+        $this->attributes['author'] = $this->author()->getResults();
+    }
 
 }
