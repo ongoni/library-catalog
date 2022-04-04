d94707b869f6edcb549797b76f12d37415157136
ongoni <ongoni.fg@gmail.com>
2022-04-04 05:52:58 +0400

    add create-store implementation for both entities

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -66,4 +66,11 @@ class Author extends Model
             );
     }
 
+    public function getFullNameAttribute()
+    {
+        return $this->first_name . ' '
+            . ($this->patronymic ? $this->patronymic . ' ' : '')
+            . $this->last_name;
+    }
+
 }
---
+++ resources/views/author/create.blade.php
@@ -0,0 +1,20 @@
+@extends('layout')
+
+@section('content')
+    <form method="POST" action="{{ route('author.store') }}">
+        @csrf
+        <div class="form-group">
+            <label for="formGroupFirstName"></label>
+            <input type="text" class="form-control" id="formGroupFirstName" name="first_name" placeholder="Имя">
+        </div>
+        <div class="form-group">
+            <label for="formGroupLastName"></label>
+            <input type="text" class="form-control" id="formGroupLastName" name="last_name" placeholder="Фамилия">
+        </div>
+        <div class="form-group">
+            <label for="formGroupPatronymic"></label>
+            <input type="text" class="form-control" id="formGroupPatronymic" name="patronymic" placeholder="Отчество">
+        </div>
+        <button type="submit" class="col-12 mt-4 btn btn-outline-dark">Добавить</button>
+    </form>
+@stop
